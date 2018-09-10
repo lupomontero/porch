abac77b6f9dfda3dd9344595e5abe4becb8e6d76
Lupo Montero <lupomontero@gmail.com>
2018-09-10 16:25:45 -0500

    updates deps and fixes issues with newer eslint version

--- test/common.js
+++ test/common.js
@@ -1,16 +1,15 @@
-exports.createDelayedPromise = (val, delay) =>
-  new Promise(resolve => setTimeout(() => resolve(val), delay));
+exports.createDelayedPromise = (val, delay) => new Promise(
+  resolve => setTimeout(() => resolve(val), delay),
+);
 
 
-exports.createFailedDelayedPromise = (err, delay) =>
-  new Promise(
-    (resolve, reject) =>
-      setTimeout(
-        () => reject((err instanceof Error) ? err : new Error(`Error: ${err}`)),
-        delay,
-      ),
-  );
+exports.createFailedDelayedPromise = (err, delay) => new Promise(
+  (resolve, reject) => setTimeout(
+    () => reject((err instanceof Error) ? err : new Error(`Error: ${err}`)),
+    delay,
+  ),
+);
 
 
-exports.timed = (fn, start = Date.now()) => (...args) =>
-  fn(...args).then(results => [results, Date.now() - start]);
+exports.timed = (fn, start = Date.now()) => (...args) => fn(...args)
+  .then(results => [results, Date.now() - start]);
